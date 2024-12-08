4e0e6acdd6d66142baa767f1f15ab0439e317514
Chinta NSR Shanmukha Teja <70648973+shanmukhateja80@users.noreply.github.com>
2024-12-08 17:23:18 +0530

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -139,8 +139,8 @@ const Navbar = () => {
     <Nav>
       <NavbarContainer>
         <NavLogo to="/">
-          <ColorText>&lt;</ColorText>Rishav
-          <div style={{ color: theme.primary }}>/</div>Chanda
+          <ColorText>&lt;</ColorText>Chinta NSR 
+          <div style={{ color: theme.primary }}>/</div>Shanmukha Teja
           <ColorText>&gt;</ColorText>
         </NavLogo>
 
